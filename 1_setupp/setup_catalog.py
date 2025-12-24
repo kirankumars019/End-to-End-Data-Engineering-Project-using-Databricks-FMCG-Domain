f7c54e8ba395717065405064e95881f62b08d0ad
Kiran Kumar S <59387829+kirankumars019@users.noreply.github.com>
2025-12-24 14:23:00 +0530

    Add files via upload

---
+++ 1_setupp/setup_catalog.py
@@ -0,0 +1,21 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC
+# MAGIC CREATE CATALOG IF NOT EXISTS fmcg;
+# MAGIC USE CATALOG fmcg;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC CREATE SCHEMA IF NOT EXISTS fmcg.gold;
+# MAGIC CREATE SCHEMA IF NOT EXISTS fmcg.silver;
+# MAGIC CREATE SCHEMA IF NOT EXISTS fmcg.bronze;
+# MAGIC
+
+# COMMAND ----------
+
+select count(*) from fmcg.gold.fact_orders; 
+
+# COMMAND ----------
+
